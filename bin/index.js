fcf1d8fee45d2a8e3391514675a074233ba19435
Ibnu Putra <azxarblog@gmail.com>
2023-06-11 16:51:47 +0700

    update how bot response

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,5 @@
 const { Meow } = require("../meow");
 const gptFunc = require("./gpt");
-const { Words } = require("./misc");
 
 //MESSAGE HANDLER
 const Handler = async (client, message) => {
@@ -17,13 +16,27 @@ const Handler = async (client, message) => {
     return await message.reply(content, options);
   }
 
-  if (cat.isValidMessage) {
+  // Handle on received personal message
+  if (!cat.isGroup) {
     await message.react("😼");
-    if (cat.value.length > 0) {
-      await gptFunc(client.pupBrowser, cat.value, async (res) => {
-        replyMessage(
-          res == "ERROR" ? "Yah cape deh maaf ya mohon coba lagi!" : res
-        );
+    await gptFunc(client.pupBrowser, message.body, async (res) => {
+      replyMessage(res == "ERROR" ? "Meow coba lagi ya😿" : res);
+
+      if (res == "ERROR") {
+        await message.react("😿");
+      } else {
+        await message.react("😽");
+      }
+    });
+  }
+
+  // Handle on bot mentioned on grup
+  if (cat.isBotMentioned) {
+    await message.react("😼");
+    const prompt = message.body.split(" ").slice(1).join(" ");
+    if (prompt.length > 0) {
+      await gptFunc(client.pupBrowser, prompt, async (res) => {
+        replyMessage(res == "ERROR" ? "Meow coba lagi ya😿" : res);
 
         if (res == "ERROR") {
           await message.react("😿");
@@ -33,31 +46,7 @@ const Handler = async (client, message) => {
       });
     } else {
       await message.react("😾");
-      replyMessage("Kasih pertanyaan yang bener yaa maniezzz!!");
-    }
-  } else {
-    if (cat.isBotMentioned) {
-      await message.react("😼");
-      const prompt = message.body
-        .split(" ")
-        .slice(1, message.body.length)
-        .join(" ");
-      if (prompt.length > 0) {
-        await gptFunc(client.pupBrowser, prompt, async (res) => {
-          replyMessage(
-            res == "ERROR" ? "Yah cape deh maaf ya mohon coba lagi!" : res
-          );
-
-          if (res == "ERROR") {
-            await message.react("😿");
-          } else {
-            await message.react("😽");
-          }
-        });
-      } else {
-        await message.react("😾");
-        replyMessage("Kasih pertanyaan yang bener yaa maniezzz!!");
-      }
+      replyMessage("Manggil doang nanya kaga😾");
     }
   }
 };
